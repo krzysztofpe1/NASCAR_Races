099f7b596c57fd396deed7fec0a2c0be925af884
krzysiek581234 <krzysztof.madajczakbiznes@gmail.com>
2023-04-05 19:19:24 +0200

    Add air Resistance and how centrifugal Force Affect car during the turn

--- NASCAR_Races/Physics.cs
+++ NASCAR_Races/Physics.cs
@@ -3,7 +3,10 @@
     internal class Physics
     {
         const float accelerationOfGravity = 9.81;
-        const float trackAngle = 9.81;
+        const float trackAngle = 10;
+        const float airDensity = 1.225;
+        const float frontSurface = 2.5;
+        const float carAirDynamic = 0.35;
 
         //sila odsrodkowa
         internal static float centrifugalForce(float speed, float radius, float mass)
@@ -14,6 +17,17 @@
         internal static float frictionForce(float mass, float frictionofweels)
         {
             return mass * frictionofweels * accelerationOfGravity * trackAngle;
+            //To DO include trackAngle 
+        }
+        //Opór powietrza
+        internal static float airResistance(float speed)
+        {
+            return 0.5 * airDensity * pow(speed, 2) * carAirDynamic * frontSurface;
+        }
+        // ile samochód się oddala/przybliża do środka
+        internal static float iscentrifugalForceEffectig(float speed, float radius, float mass, float frictionofweels)
+        {
+            return (frictionForce(mass, frictionofweels) - centrifugalForce(speed, radius, mass)) / mass;
         }
         internal static float Street(float speed)
         {
